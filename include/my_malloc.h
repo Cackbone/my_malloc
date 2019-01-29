ce02b6373785eacd4fbadbb710c326abf783eb2e
Jonathan Lemoine <jonathan.lemoine@epitech.eu>
2019-01-29 13:57:29 +0100

    (feat) [free]: (WIP) Add free

--- include/my_malloc.h
+++ include/my_malloc.h
@@ -13,11 +13,12 @@
 // Get the number of block needed to reserve x bytes in memory
 #define ALIGN(x) ((x + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1))
 
+extern void *START_MEM_PTR;
+
 typedef struct mem_block_s
 {
     size_t size;
     char free;
-    char offset;
 } mem_block_t;
 
 void *malloc(size_t);
--- src/malloc.c
+++ src/malloc.c
@@ -13,36 +13,28 @@ static void *get_free_space(size_t size)
 
     while (it < top) {
         header = it;
-        if (header->free && header->size <= size)
+        if (header->free && header->size >= size)
             return it;
-        it += sizeof(mem_block_t) + header->size + header->offset;
+        it += sizeof(mem_block_t) + header->size;
     }
     return NULL;
 }
 
-char get_alignment(void *ptr)
-{
-    const size_t ptr_addr = (size_t)ptr;
-    return ((ptr_addr + sizeof(mem_block_t)) >>
-            (BLOCK_SIZE >> 1)) << (BLOCK_SIZE >> 1);
-}
-
 void split_free_space(void *ptr, int size)
 {
-    mem_block_t *free_space;
+    mem_block_t *free_space = NULL;
     mem_block_t *header = ptr;
     size_t remaining_space = header->size - size;
-    mem_block_t new_block = {remaining_space,
-                             1,
-                             get_alignment(ptr + sizeof(mem_block_t) +
-                                           header->offset + header->size)
-    };
+    mem_block_t new_block = {remaining_space, 1};
 
     if (remaining_space <= sizeof(mem_block_t))
         return;
     header->size = size;
-    free_space = (mem_block_t *)(ptr + sizeof(mem_block_t) + header->offset + header->size);
+    write(1, "1", 1);
+    free_space = (mem_block_t *)(ptr + sizeof(mem_block_t) + header->size);
+    write(1, "2", 1);
     *free_space = new_block;
+    write(1, "3", 1);
 }
 
 void *malloc(size_t size)
@@ -51,24 +43,55 @@ void *malloc(size_t size)
     mem_block_t block;
     void *ptr = NULL;
 
+    if (size == 0)
+	return NULL;
     if (!START_MEM_PTR)
         START_MEM_PTR = sbrk(0);
-    free = get_free_space(size);
+    free = get_free_space(ALIGN(size));
     if (!free) {
-        block.size = size;
+        block.size = ALIGN(size);
         block.free = 0;
-        block.offset = get_alignment(sbrk(0));
-        ptr = sbrk(sizeof(mem_block_t) + block.size + block.offset);
+        ptr = sbrk(sizeof(mem_block_t) + block.size);
         *((mem_block_t *)ptr) = block;
-        return (ptr + sizeof(mem_block_t) + block.offset);
+	return (ptr + sizeof(mem_block_t));
     } else {
-        split_free_space(free, size);
-        block.size = size;
+	write(1, "a", 1);
+        split_free_space(free, ALIGN(size));
+	write(1, "b", 1);
+        block.size = ALIGN(size);
         block.free = 0;
-        block.offset = get_alignment(free);
         *((mem_block_t *)free) = block;
-        return (free + sizeof(mem_block_t) + block.offset);
+        return (free + sizeof(mem_block_t));
     }
 }
 
-void free(__attribute__((unused))void *ptr) {}
+
+static void show_alloc_mem()
+{
+    void *it = START_MEM_PTR;
+    void *top = sbrk(0);
+    mem_block_t *header = NULL;
+    
+    printf("break : %p\n", START_MEM_PTR);
+    while (it < top) {
+	header = it;
+	printf("%p - %p : %ld - %d\n",
+	       it + sizeof(mem_block_t),
+	       it + sizeof(mem_block_t) + header->size,
+	       header->size,
+	       header->free);
+	it += sizeof(mem_block_t) + header->size;
+    }
+}
+
+
+void free(void *ptr)
+{
+    if (!ptr) {
+    	show_alloc_mem();
+	return;
+    }
+    mem_block_t *block = ptr - sizeof(mem_block_t);
+
+    block->free = 1;
+}
