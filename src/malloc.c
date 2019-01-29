7d67cf3d27d43d563bd3859649fc7f12e91f777e
Jonathan Lemoine <jonathan.lemoine@epitech.eu>
2019-01-29 23:50:48 +0100

    (feat) [malloc/free]: (WIP) Add pagesize

--- src/malloc.c
+++ src/malloc.c
@@ -42,7 +42,7 @@ void print_nbr_err(size_t nb)
     }
 }
 
-static void show_alloc_mem()
+void show_alloc_mem()
 {
     void *it = START_MEM_PTR;
     void *top = sbrk(0);
@@ -67,8 +67,8 @@ static void show_alloc_mem()
 	write(1, "\n", 1);
 	if (stop)
 	    exit(1);
-	/* if (header->size == 0) */
-	/*     stop = 1; */
+	if (header->size == 0)
+	    stop = 1;
 	it += sizeof(mem_block_t) + header->size;
     }
 }
@@ -92,18 +92,8 @@ static void *get_free_space(size_t size)
 
     while (it < top) {
         header = it;
-	/* putstr("size: "); */
-	/* print_nbr(header->size); */
-	/* putstr("\tit: "); */
-	/* print_nbr((size_t)it); */
-	/* putstr("\n"); */
         if (header->free && header->size >= size)
             return it;
-	if (header->size == 0) {
-	    //putstr("\n\n\n\n");
-	    show_alloc_mem();
-	    //exit(1);
-	}
         it += sizeof(mem_block_t) + header->size;
     }
     return NULL;
@@ -116,23 +106,10 @@ void split_free_space(void *ptr, int size)
     size_t remaining_space = header->size - size;
     mem_block_t new_block = {remaining_space - sizeof(mem_block_t), 1};
 
-    putstr("Remaining space: ");
-    print_nbr(remaining_space);
-    putstr("\n");
     if (remaining_space <= sizeof(mem_block_t))
         return;
-    putstr("[split] Create new block of size ");
-    print_nbr(remaining_space - sizeof(mem_block_t));
-    putstr("\n");
-    print_split_debug(header->size, size, remaining_space - sizeof(mem_block_t));
     header->size = size;
-    putstr("[split] Resize block to ");
-    print_nbr(size);
-    putstr("\n");
     free_space = (ptr + sizeof(mem_block_t) + size);
-    putstr("[Create block]: ");
-    print_nbr((size_t)((mem_block_t *)free_space));
-    putstr("\n");
     *((mem_block_t *)free_space) = new_block;
 }
 
@@ -145,33 +122,26 @@ void *malloc(size_t size)
 
     if (size == 0)
 	return NULL;
-    putstr("malloc(");
-    print_nbr(size);
-    putstr(");\n");
     if (!START_MEM_PTR)
         START_MEM_PTR = sbrk(0);
     free = get_free_space(ALIGN(size));
-    if (!free) {
-	putstr("No space available\n");
+    if (free == 0) {
 	if (size % pagesize == 0)
 	    block.size = ((size / pagesize)) * pagesize;
 	else
 	    block.size = ((size / pagesize) + 1) * pagesize;
         block.free = 1;
+	putstr("Create block of size: ");
+	print_nbr(block.size);
+	putstr("\n");
         ptr = sbrk(sizeof(mem_block_t) + block.size);
 	if (ptr == (void *)-1)
 	    return NULL;
         *((mem_block_t *)ptr) = block;
-	putstr("[Create block]: ");
-	print_nbr((size_t)((mem_block_t *)ptr));
-	putstr("\n");
 	return (malloc(size));
     } else {
         split_free_space(free, ALIGN(size));
 	((mem_block_t *)free)->free = 0;
-	putstr("[malloc] Create new block of size ");
-	print_nbr(ALIGN(size));
-	putstr("\n");
         return (free + sizeof(mem_block_t));
     }
 }
@@ -179,6 +149,7 @@ void *malloc(size_t size)
 void free(void *ptr)
 {
     if (!ptr) {
+	show_alloc_mem();
 	return;
     }
     mem_block_t *block = ptr - sizeof(mem_block_t);
