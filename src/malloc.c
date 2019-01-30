e6a5bb66c41ade347726171e163e3bbaae6a89ea
Jonathan Lemoine <jonathan.lemoine@epitech.eu>
2019-01-30 18:35:22 +0100

    (feat) [malloc/free]: Add free

--- src/malloc.c
+++ src/malloc.c
@@ -138,13 +138,11 @@ void *malloc(size_t size)
 	else
 	    block.size = ((size / (pagesize - sizeof(mem_block_t))) + 1) * pagesize;
         block.free = 1;
-	print_nbr(block.size);
-	putstr("\n");
         ptr = sbrk(block.size);
+	block.size -= sizeof(mem_block_t);
 	if (ptr == (void *)-1)
 	    return NULL;
         *((mem_block_t *)ptr) = block;
-	show_alloc_mem();
         return malloc(size);
     } else {
         split_free_space(free, ALIGN(size));
@@ -153,15 +151,45 @@ void *malloc(size_t size)
     }
 }
 
+mem_block_t *merge_blocks()
+{
+    void *it = START_MEM_PTR;
+    void *top = sbrk(0);
+    mem_block_t *block = NULL;
+    mem_block_t *next = NULL;
+    mem_block_t *last = NULL;
+
+    while ((size_t)it < (size_t)top) {
+	block = it;
+	next = it + block->size + sizeof(mem_block_t);
+        if (block->free && (size_t)next < (size_t)top && next->free) {
+	    block->size += sizeof(mem_block_t) + next->size;
+	} else {
+	    last = it;
+	    it += block->size + sizeof(mem_block_t);
+	}
+    }
+    return last;
+}
+
 void free(void *ptr)
 {
+    mem_block_t *block = ptr - sizeof(mem_block_t);
+    mem_block_t *last = NULL;
+    size_t pagesize = getpagesize();
+
     if (!ptr) {
-	show_alloc_mem();
+	//show_alloc_mem();
 	return;
     }
-    mem_block_t *block = ptr - sizeof(mem_block_t);
 
     block->free = 1;
+    last = merge_blocks();
+
+    if (last->free && last->size >= 2 * pagesize) {
+	sbrk(-((last->size / pagesize - 1) * pagesize));
+	last->size -= (last->size / pagesize - 1) * pagesize;
+    }
 }
 
 void *calloc(size_t nmemb, size_t size)
